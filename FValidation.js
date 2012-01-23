2aadc446cdfd0a6526fb40e5022e7faa5b2350cd
Ferso <erickfernando@gmail.com>
2012-01-23 04:00:52 -0600

    Find by 'Required' HTML5 attribute

--- FValidation.js
+++ FValidation.js
@@ -3,6 +3,7 @@
 	$.fn.Validation = function(options) {		
 		var defaults = { 			 
 			 autosubmit: true,
+			 onBefore:function(){},
 			 onError:function(){},
 			 onSuccess : function(){}
 		}
@@ -11,38 +12,51 @@
 				
 		this.form = this;		
 		var that  = this;		
-		this.form.bind("submit", function(){			
+		this.form.bind("submit", function(){					
 			return that.Submit();				
-		});
+		});	
+		
+		return this;
 	
 	},
 	
 	$.fn.Submit = function( ){	
-			var collection = $( '.required' ) ;
-			var error =  new Array(); 			
-			$.each(collection, function(i,e){				
-				if( e.value == '' ){						
-					error.push( e );
-				}					
-			});
-			
-			if( error.length == 0 ){				
-					this.options.onSuccess.call(this,collection);					
-					if( this.options.autosubmit ){								
-						return true;						
-					}else{						
-						return false;
-					}
-			}else{
-				
-				$.each(error,function(i,e){						
-					$(e).addClass('errorInput');						
-				});
-				
-				this.options.onError.call(this,error);					
-				return false;				
+		
+		this.options.onBefore.call(this,this);		
+		
+		if( this.isValid() ){						
+				this.options.onSuccess.call(this,this.collection);					
+				if( this.options.autosubmit ){								
+					return true;						
+				}else{						
+					return false;
+				}
 			}			
 		return false;			
+	},
+	
+	$.fn.isValid = function(){
+		
+		var that = this;		
+		this.collection = $("input[required=required]") ;		
+		this.error =  new Array(); 	
+		
+		$.each( this.collection, function(i,e){				
+			if( e.value == '' ){												
+				that.error.push( e );
+			}					
+		});		
+		
+		if( that.error.length == 0 ){				
+			return true;		
+		}else{				
+			$.each(this.error,function(i,e){						
+				$(e).addClass('errorInput');						
+			});			
+			this.options.onError.call(this,this.error);					
+			return false;				
+		}					
+		return false;		
 	}
 		
 })(jQuery);  
